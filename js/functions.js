fc96db6030f2fcd333c1f04b3460f59e39554e97
Ekaterina Nesterenko <89526050507@mail.ru>
2024-12-16 17:27:15 +0300

    add 2 functions

---
+++ js/functions.js
@@ -0,0 +1,15 @@
+function checkStringLength(string, maxLength) {
+  string.length <= maxLength;
+}
+
+checkStringLength();
+
+function checkPalindrom(string) {
+  const normalisedString = string.replaceAll(' ', '').toLowerCase();
+  let reversedString = '';
+  for (let i = normalisedString.length - 1; i >=0; i--){
+    reversedString +=normalisedString[i];
+  }
+  return normalisedString === reversedString;
+}
+checkPalindrom();
