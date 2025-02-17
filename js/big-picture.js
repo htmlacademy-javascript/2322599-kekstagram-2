5471dc60654593147105a96cfed67b93d1d7ad2f
Ekaterina Nesterenko <89526050507@mail.ru>
2025-02-17 23:25:58 +0300

    Открывается и закрывается (часть 2)

--- js/big-picture.js
+++ js/big-picture.js
@@ -3,7 +3,15 @@ import { isEscapeKey } from './util.js';
 const bigPicture = document.querySelector('.big-picture');
 const closeButton = bigPicture.querySelector('.big-picture__cancel');
 const bodyElement = document.body;
+const commentsLoader = bigPicture.querySelector('.comments-loader');
+const commentsShownCount = bigPicture.querySelector('.social__comment-shown-count');
+const commentsTotalCount = bigPicture.querySelector('.social__comment-total-count');
+const socialComments = bigPicture.querySelector('.social__comments');
 
+let currentCommentIndex = 0;
+let COMMENTS_STEP = 5;
+
+// Закрывает модальное окно
 function onCloseButtonClick() {
   bigPicture.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
@@ -12,6 +20,7 @@ function onCloseButtonClick() {
   document.removeEventListener('keydown', onDocumentKeydown);
 }
 
+// Закрыввет при нажатии Esc
 function onDocumentKeydown(evt) {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
@@ -19,41 +28,50 @@ function onDocumentKeydown(evt) {
   }
 }
 
-closeButton.addEventListener('click', onCloseButtonClick);
-document.addEventListener('keydown', onDocumentKeydown);
-
-const openBigPicture = (photo) => {
-  const bigPictureImage = bigPicture.querySelector('.big-picture__img img');
-  const socialCaption = bigPicture.querySelector('.social__caption');
-  const likesCount = bigPicture.querySelector('.likes-count');
-  const commentsShownCount = bigPicture.querySelector(
-    '.social__comment-shown-count'
-  );
-  const commentsTotalCount = bigPicture.querySelector(
-    '.social__comment-total-count'
-  );
-  const socialComments = bigPicture.querySelector('.social__comments');
+// Отображает комментарии
+function renderComments(comments) {
+  const commentsToShow = comments.slice(currentCommentIndex, currentCommentIndex + COMMENTS_STEP);
 
   socialComments.innerHTML = '';
 
+  commentsToShow.forEach(comment => {
+    const commentElement = document.createElement('li');
+
+    commentElement.classList.add("social__comment");
+    commentElement.innerHTML =
+      `<img class="social__picture"
+    src="${comment.avatar}"
+    alt="${comment.name}"
+    width="35"
+    height="35">
+    <p class="social__text">${comment.message}</p>`;
+    socialComments.appendChild(commentElement);
+  });
+
+  commentsShownCount.textContent = Math.min(currentCommentIndex + COMMENTS_STEP, comments.length);
+  commentsTotalCount.textContent = comments.length;
+
+  commentsLoader.classList.toggle('hidden', currentCommentIndex + COMMENTS_STEP >= comments.length);
+}
+
+// Открывает большое фото
+function openBigPicture(photo) {
+  const bigPictureImage = bigPicture.querySelector('.big-picture__img img');
+  const socialCaption = bigPicture.querySelector('.social__caption');
+  const likesCount = bigPicture.querySelector('.likes-count');
+
+  currentCommentIndex = 0;
+
   bigPictureImage.src = photo.url;
   likesCount.textContent = photo.likes;
-  commentsShownCount.textContent = photo.comments.length;
-  commentsTotalCount.textContent = photo.comments.length;
   socialCaption.textContent = photo.description;
 
-  const commentsList = photo.comments.reduce((accumulator, comment) =>
-    `${accumulator}
-    <li class="social__comment">
-      <img class="social__picture"
-        src="${comment.avatar}"
-        alt="${comment.name}"
-        width="35"
-        height="35">
-        <p class="social__text">${comment.message}</p>
-    </li>`, '');
+  renderComments(photo.comments);
 
-  socialComments.innerHTML = commentsList;
+  commentsLoader.onclick = () => {
+    currentCommentIndex += COMMENTS_STEP;
+    renderComments(photo.comments);
+  };
 
   bigPicture.classList.remove('hidden');
   bodyElement.classList.add('modal-open');
